9e70105e67bdd496ba00de19b80504dd9220ba1c
Ruslan <shamiltat79@gmail.com>
2022-11-04 01:31:16 +0200

    hw8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
 // Change code below this line
 
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 console.log(galleryItems);
+const galleryToAdd = document.querySelector('.gallery');
+const galleryItem = createGalleryItem(galleryItems); 
+galleryToAdd.insertAdjacentHTML('beforeend', galleryItem);
+function createGalleryItem(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>` })
+        .join('');
+
+    
+}
+
+let lightBox = new SimpleLightbox('.gallery a', {
+        captionsData: "alt",
+        captionDelay: 250
+        
+    });
+
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
 
+const player = new Player('handstick', {
+    id: 19231868,
+    width: 640
+});
+
+player.on('play', function() {
+    console.log('played the video!');
+});
+const onPlay = function () {
+    duration: 61.857
+    percent: 0.049
+    seconds: 3.034
+    // data is an object containing properties specific to that event
+};
+
+player.on('timeupdate', onPlay);
+
+    
